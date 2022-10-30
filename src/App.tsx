55699cb7ec36881339fec1cf4cf7797efdce871a
m2mt-l <ta28ma2@gmail.com>
2022-10-30 14:29:51 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./components/Main";
+
+const App: React.FunctionComponent = () => {
+    return <Main />;
+};
+
+export default App;
