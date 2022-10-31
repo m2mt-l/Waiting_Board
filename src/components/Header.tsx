512765c98c3bfe8dd637071a4b865576493e8e55
m2mt-l <ta28ma2@gmail.com>
2022-10-31 11:31:20 +0000

    style: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
+import Typography from "@mui/material/Typography";
 
 const Header: FC = () => {
-    return <div>Header</div>;
+    return (
+        <Typography variant="h3" my={3} component="div">
+            Waiting Board
+        </Typography>
+    );
 };
 
 export default Header;
